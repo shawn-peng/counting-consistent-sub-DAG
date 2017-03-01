bc7dbc3a7c6087b7b37976628092b3de1f06ff76
Shawn Peng <yisupeng@bolt.cs.indiana.edu>
2017-02-28 23:29:07 -0500

    add used to script

--- run_data.sh
+++ run_data.sh
@@ -7,5 +7,6 @@ if [ -z "$dag" -o -z "$level" ]; then
 echo "Usage: $0 <mfo|cco|bpo> <level>"
 fi
 
-./graph_alg data/gene_ontology/$dag/all_$dag$level_rel.txt > log/${dag}${level}_all.log
+./graph_alg data/gene_ontology/$dag/all_$dag$level_rel.txt > log/${dag}${level}_all.log &
+./graph_alg data/gene_ontology/$dag/used_$dag$level_rel.txt > log/${dag}${level}_used.log &
 
