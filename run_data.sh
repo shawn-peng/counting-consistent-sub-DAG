ee19605e328128f2adf09bc13b213f32955b700f
Shawn Peng <yisupeng@bolt.cs.indiana.edu>
2017-02-28 23:30:03 -0500

    exit when no args

--- run_data.sh
+++ run_data.sh
@@ -5,6 +5,7 @@ level="$2"
 
 if [ -z "$dag" -o -z "$level" ]; then
 echo "Usage: $0 <mfo|cco|bpo> <level>"
+exit 1
 fi
 
 ./graph_alg data/gene_ontology/$dag/all_$dag$level_rel.txt > log/${dag}${level}_all.log &
