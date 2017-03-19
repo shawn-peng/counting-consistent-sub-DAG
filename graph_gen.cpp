08d887d3ebc74e0bc86060367fd1e07335a45880
Shawn Peng <yisupeng@bolt.cs.indiana.edu>
2017-03-19 17:43:15 -0400

    remove real dist

--- graph_gen.cpp
+++ graph_gen.cpp
@@ -12,7 +12,7 @@
 #include <stdio.h>
 
 #include <boost/random.hpp>
-#include <boost/random/uniform_real_distribution.hpp>
+//#include <boost/random/uniform_real_distribution.hpp>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/variate_generator.hpp>
 #include <boost/math/distributions/pareto.hpp>
@@ -23,7 +23,7 @@ using namespace NS_DAG;
 //static std::random_device rd;
 static boost::mt19937 randGen(std::time(0)); //held constant for repeatability
 static boost::exponential_distribution<> dist;
-static boost::random::uniform_real_distribution<> uniformReal(1.0,10.0); //this range can be adjusted to effect values
+//static boost::random::uniform_real_distribution<> uniformReal(1.0,10.0); //this range can be adjusted to effect values
 static boost::variate_generator<boost::mt19937&, boost::exponential_distribution<> > generator(randGen, dist);
 
 int paretoRand()
