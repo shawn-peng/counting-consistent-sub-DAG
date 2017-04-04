880e04eec1afac075da9de0480486a58d6e8a611
Shawn Peng <yisupeng@bolt.cs.indiana.edu>
2017-04-04 16:57:26 -0400

    add script to grep then tail grep the log with level

---
+++ tail_log.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+log=$1
+if [ -z "$log" ]; then
+	echo "Usage: $0 <log-file> [show-recursive-level]"
+fi
+
+level=$2
+
+if [ -z "$level" ]; then
+	level=8
+fi
+
+grep "\[[1-${level}]\]" ${log} && tail -f ${log} |grep "\[[1-${level}]\]"
+
