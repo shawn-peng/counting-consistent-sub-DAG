943e7c56d4ac11956007b3ac27ca994c47c83a68
Shawn Peng <yisupeng@bolt.cs.indiana.edu>
2017-04-04 16:48:12 -0400

    add categ option for run data

--- run_data.sh
+++ run_data.sh
@@ -2,12 +2,20 @@
 
 dag="$1"
 level="$2"
+categ="$3"
 
 if [ -z "$dag" -o -z "$level" ]; then
-echo "Usage: $0 <mfo|cco|bpo> <level>"
+echo "Usage: $0 <mfo|cco|bpo> <level> [used|all]"
 exit 1
 fi
 
-{ time ./graph_alg data/gene_ontology/$dag/all_${dag}${level}_rel.txt; } &> log/${dag}${level}_all.log &
-{ time ./graph_alg data/gene_ontology/$dag/used_${dag}${level}_rel.txt; } &> log/${dag}${level}_used.log &
+if [ -z "$categ" ]; then
+	#run both
+	{ time ./graph_alg data/gene_ontology/$dag/all_${dag}${level}_rel.txt; } &> log/${dag}${level}_all.log &
+	{ time ./graph_alg data/gene_ontology/$dag/used_${dag}${level}_rel.txt; } &> log/${dag}${level}_used.log &
+elif [[ "$categ" == "used" ]]; then
+	{ time ./graph_alg data/gene_ontology/$dag/used_${dag}${level}_rel.txt; } &> log/${dag}${level}_used.log &
+elif [[ "$categ" == "all" ]]; then
+	{ time ./graph_alg data/gene_ontology/$dag/all_${dag}${level}_rel.txt; } &> log/${dag}${level}_all.log &
+fi
 
