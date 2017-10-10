363b3b163469d7b914da3e1ee8d4832fd1deeceb
Yisu Peng <yisupeng@gmail.com>
2017-10-10 14:30:15 -0400

    experiment scripts

---
+++ exp_prune_hash_flowpivot.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#datadir="data/simulated_dags/150_5/"
+. exp_conf
+
+result_file="experiments/methods/baseline.csv"
+detail_log_dir="experiments/methods/baseline_details/"
+	
+mkdir -p $detail_log_dir
+
+usedtimes=()
+for g in `ls $datadir`; do
+	echo $g
+	echo "file	count	time(ms)	recursive_calls" > $result_file
+
+	#fname=`echo $g | grep -oP "[^/]*$"`
+	#echo $fname
+
+	./graph_alg ${datadir}${g} --prune --hash --pivot flow > ${detail_log_dir}${g}
+
+	t=`cat ${detail_log_dir}${g} | sed -ne 's/time: \([0-9]*\)ms/\1/p'`
+	
+	usedtimes+=($t)
+done
+
+echo $usedtimes
+
