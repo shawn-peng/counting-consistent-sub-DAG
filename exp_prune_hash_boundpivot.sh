d96554cf1b61855981caac46db30625250062c4f
Yisu Peng <yisupeng@gmail.com>
2017-10-12 09:32:47 -0400

    add several new exp scripts

---
+++ exp_prune_hash_boundpivot.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#datadir="data/simulated_dags/150_5/"
+. exp_conf
+
+result_file="experiments/pivots/prune_hash_boundpivot.csv"
+detail_log_dir="experiments/pivots/prune_hash_boundpivot_details/"
+
+alg_parameters="--prune --hash --pivot bound"
+	
+mkdir -p $detail_log_dir
+
+echo "file	count	time(ms)	recursive_calls" > $result_file
+
+usedtimes=()
+for g in `ls $datadir`; do
+	echo $g
+
+	#fname=`echo $g | grep -oP "[^/]*$"`
+	#echo $fname
+
+	./graph_alg ${datadir}${g} ${alg_parameters} > ${detail_log_dir}${g}
+
+	t=`cat ${detail_log_dir}${g} | sed -ne 's/time: \([0-9]*\)ms/\1/p'`
+	
+	usedtimes+=($t)
+done
+
+echo $usedtimes
+
