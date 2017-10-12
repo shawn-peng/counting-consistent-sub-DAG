d3cf2e8eb464daad431368bc09be9c2395d4a4ab
Yisu Peng <yisupeng@gmail.com>
2017-10-12 09:31:25 -0400

    update exp script

--- exp_baseline.sh
+++ exp_baseline.sh
@@ -10,10 +10,11 @@ mkdir -p $detail_log_dir
 
 alg_parameters="--pivot random_mpn"
 
+echo "file	count	time(ms)	recursive_calls" > $result_file
+
 usedtimes=()
 for g in `ls $datadir`; do
 	echo $g
-	echo "file	count	time(ms)	recursive_calls" > $result_file
 
 	#fname=`echo $g | grep -oP "[^/]*$"`
 	#echo $fname
@@ -21,6 +22,10 @@ for g in `ls $datadir`; do
 	./graph_alg ${datadir}${g} ${alg_parameters} > ${detail_log_dir}${g}
 
 	t=`cat ${detail_log_dir}${g} | sed -ne 's/time: \([0-9]*\)ms/\1/p'`
+
+	cnt=`cat ${detail_log_dir}${g} | sed -ne 's/: \([0-9]*\)ms/\1/p'`
+
+	echo "$g		$t" >> $result_file
 	
 	usedtimes+=($t)
 done
