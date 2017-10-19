24a4fd3885615d92bcdd30f9db74dadcde177e73
Yisu Peng <yisupeng@gmail.com>
2017-10-18 23:54:22 -0400

    add 2 scripts to deal with results

---
+++ grep_count.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+f=$1
+
+#t=`cat ${detail_log_dir}${f} | sed -ne 's/time: \([0-9]*\)ms/\1/p'`
+#calls=`cat ${detail_log_dir}${f} | sed -ne 's/Function count_consistent_subdag_for_independent_subdag has been called \([0-9]*\) times./\1/p'`
+
+cnt=`tail -n20 ${f} | sed -ne 's/Num of consistent sub-DAG: \([0-9]*\)/\1/p'`
+
+echo cnt
+
