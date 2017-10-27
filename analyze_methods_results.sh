050f788ad9435f45c8bbc4febf14cbb2bb6a052b
Yisu Peng <yisupeng@gmail.com>
2017-10-27 00:11:07 -0400

    update exp analyze scripts

--- analyze_methods_results.sh
+++ analyze_methods_results.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-EXP=$1
+#EXP=$1
 
-result_file="experiments/methods/${1}.csv"
-detail_log_dir="experiments/methods/${1}_details/"
+detaildirs=`ls experiments/methods/ | grep _details`
+
+exps=${detaildirs//_details/}
+
+for EXP in $exps; do
+echo $EXP
+
+result_file="experiments/methods/${EXP}.csv"
+detail_log_dir="experiments/methods/${EXP}_details/"
 
 
 echo "file	count	time(ms)	recursive_calls" > $result_file
@@ -12,8 +19,9 @@ usedtimes=()
 totaltime=0
 totalcalls=0
 
+((i = 0))
 for f in `ls $detail_log_dir`; do
-	echo $f
+	echo -ne "$f\r"
 
 	#fname=`echo $f | grep -oP "[^/]*$"`
 	#echo $fname
@@ -21,21 +29,25 @@ for f in `ls $detail_log_dir`; do
 	t=`cat ${detail_log_dir}${f} | sed -ne 's/time: \([0-9]*\)ms/\1/p'`
 	calls=`cat ${detail_log_dir}${f} | sed -ne 's/Function count_consistent_subdag_for_independent_subdag has been called \([0-9]*\) times./\1/p'`
 
-	cnt=`cat ${detail_log_dir}${f} | sed -ne 's/Num of consistent sub-DAG: \([0-9]*\)/\1/p'`
+	cnt=`cat ${detail_log_dir}${f} | sed -ne 's/^Num of consistent sub-DAG: \([0-9]*\)/\1/p'`
 
 	echo "$f	$cnt	$t	$calls" >> $result_file
 	
 	usedtimes+=($t)
 	((totaltime+=t))
 	((totalcalls+=calls))
+
+	echo -ne "\033[K"
 done
 
 ((avgtime = totaltime/1000))
 echo $totaltime
-echo $avgtime
+#echo $avgtime
 
 ((avgcalls = totalcalls/1000))
 echo $totalcalls
-echo $avgcalls
+#echo $avgcalls
 
+done
+exit
 
