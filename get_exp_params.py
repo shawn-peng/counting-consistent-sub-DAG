81b2b2f1329e6a76a4e13ee4609723899b3c194a
Yisu Peng <yisupeng@gmail.com>
2017-10-28 22:19:54 -0400

    get_exp_params.py add new args for reverse

--- get_exp_params.py
+++ get_exp_params.py
@@ -5,27 +5,32 @@ import sys
 exp_name = sys.argv[1];
 
 exp_dict = {
-	"bruteforce"				: "--brute-force",
+	"bruteforce"					: "--brute-force",
 
-	"randpivot"					: "--pivot random",
-	"randpivot_prune"			: "--pivot random --prune",
-	"randpivot_prune_hash"		: "--pivot random --prune --hash",
+	"randpivot"						: "--pivot random",
+	"randpivot_prune"				: "--pivot random --prune",
+	"randpivot_prune_hash"			: "--pivot random --prune --hash",
+	"randpivot_prune_hash_reverse"	: "--pivot random --prune --hash --allow-reverse",
 
-	"mpnpivot"					: "--pivot random_mpn",
-	"mpnpivot_prune"			: "--pivot random_mpn --prune",
-	"mpnpivot_prune_hash"		: "--pivot random_mpn --prune --hash",
+	"mpnpivot"						: "--pivot random_mpn",
+	"mpnpivot_prune"				: "--pivot random_mpn --prune",
+	"mpnpivot_prune_hash"			: "--pivot random_mpn --prune --hash",
+	"mpnpivot_prune_hash_reverse"	: "--pivot random_mpn --prune --hash --allow-reverse",
 
-	"flowpivot"					: "--pivot flow",
-	"flowpivot_prune"			: "--pivot flow --prune",
-	"flowpivot_prune_hash"		: "--pivot flow --prune --hash",
+	"flowpivot"						: "--pivot flow",
+	"flowpivot_prune"				: "--pivot flow --prune",
+	"flowpivot_prune_hash"			: "--pivot flow --prune --hash",
+	"flowpivot_prune_hash_reverse"	: "--pivot flow --prune --hash --allow-reverse",
 
-	"boundpivot"				: "--pivot bound",
-	"boundpivot_prune"			: "--pivot bound --prune",
-	"boundpivot_prune_hash"		: "--pivot bound --prune --hash",
+	"boundpivot"					: "--pivot bound",
+	"boundpivot_prune"				: "--pivot bound --prune",
+	"boundpivot_prune_hash"			: "--pivot bound --prune --hash",
+	"boundpivot_prune_hash_reverse"	: "--pivot bound --prune --hash --allow-reverse",
 
-	"degreepivot"				: "--pivot degree",
-	"degreepivot_prune"			: "--pivot degree --prune",
-	"degreepivot_prune_hash"	: "--pivot degree --prune --hash",
+	"degreepivot"					: "--pivot degree",
+	"degreepivot_prune"				: "--pivot degree --prune",
+	"degreepivot_prune_hash"		: "--pivot degree --prune --hash",
+	"degreepivot_prune_hash_reverse": "--pivot degree --prune --hash --allow-reverse",
 
 };
 
